ceab0d29619035c239de4f070c8a3b4ef5622ed5
savchenkoadd <savchenkooad@gmail.com>
2024-01-22 08:43:39 +0200

    Add essential entities

---
+++ OrdersSolution/src/Orders.Core/Entities/Orders/Order.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Core.Entities.Orders
+{
+	public class Order
+	{
+		[Key]
+		public Guid Id { get; set; }
+
+		[Required]
+		[StringLength(30)]
+		public string? OrderNumber { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string? CustomerName { get; set; }
+
+		[Required]
+		public DateTime PlacedDate { get; set; }
+
+		[Range(0, double.MaxValue)]
+		public double TotalAmount { get; set; }
+	}
+}
---
+++ OrdersSolution/src/Orders.Core/Entities/Orders/OrderItem.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Core.Entities.Orders
+{
+	public class OrderItem
+	{
+		[Key]
+		public Guid Id { get; set; }
+
+		[Required]
+		public Guid OrderId { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string? ProductName { get; set; }
+
+		[Range(0, int.MaxValue)]
+		public int Quantity { get; set; }
+
+		[Range(0, double.MaxValue)]
+		public double UnitPrice { get; set; }
+
+		public double TotalPrice { get; set; }
+	}
+}
