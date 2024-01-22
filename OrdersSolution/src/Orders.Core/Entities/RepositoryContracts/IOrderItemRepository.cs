d2d265e9eb7c3db73ef2010f41982b3c5d553410
savchenkoadd <savchenkooad@gmail.com>
2024-01-22 09:11:33 +0200

    Add Repository interfaces

---
+++ OrdersSolution/src/Orders.Core/Entities/RepositoryContracts/IOrderItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using Orders.Core.Entities.Orders;
+
+namespace Orders.Core.Entities.RepositoryContracts
+{
+	public interface IOrderItemRepository
+	{
+		Task<List<OrderItem>> GetOrderItemsByOrderId(Guid orderId);
+
+		Task<OrderItem> GetOrderItem(Guid orderItemId);
+
+		Task<int> CreateOrderItem(OrderItem orderItem);
+
+		Task<int> UpdateOrderItem(Guid orderItemId, OrderItem orderItem);
+
+		Task<int> DeleteOrderItem(Guid orderItemId);
+	}
+}
---
+++ OrdersSolution/src/Orders.Core/Entities/RepositoryContracts/IOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using Orders.Core.Entities.Orders;
+
+namespace Orders.Core.Entities.RepositoryContracts
+{
+	public interface IOrderRepository
+	{
+		Task<List<Order>?> GetAllOrders();
+
+		Task<Order?> GetOrder(Guid orderId);
+
+		Task<int> CreateOrder(Order order);
+
+		Task<int> UpdateOrder(Guid orderId, Order order);
+
+		Task<int> DeleteOrder(Guid orderId);
+	}
+}
