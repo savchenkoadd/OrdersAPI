65438c0dd8944b1d3e8f601f6cc6a958804db1af
savchenkoadd <savchenkooad@gmail.com>
2024-01-22 10:13:09 +0200

    Create services interfaces

---
+++ OrdersSolution/src/Orders.Core/ServiceContracts/IOrderItemService.cs
@@ -0,0 +1,18 @@
+﻿using Orders.Core.Domain.Entities.Orders;
+using Orders.Core.DTO.OrderItem;
+
+namespace Orders.Core.ServiceContracts
+{
+	public interface IOrderItemService
+	{
+		Task<List<OrderItemResponse>> GetOrderItemsByOrderId(Guid? orderId);
+
+		Task<OrderItemResponse> GetOrderItem(Guid? orderItemId);
+
+		Task<bool> CreateOrderItem(OrderItemAddRequest? orderItemAddRequest);
+
+		Task<bool> UpdateOrderItem(OrderItemUpdateRequest? orderItemUpdateRequest);
+
+		Task<bool> DeleteOrderItem(Guid? orderItemId);
+	}
+}
---
+++ OrdersSolution/src/Orders.Core/ServiceContracts/IOrderService.cs
@@ -0,0 +1,17 @@
+﻿using Orders.Core.DTO.Order;
+
+namespace Orders.Core.ServiceContracts
+{
+	public interface IOrderService
+	{
+		Task<List<OrderResponse>> GetAllOrders();
+
+		Task<OrderResponse> GetOrder(Guid? orderId);
+
+		Task<bool> CreateOrder(OrderAddRequest? orderAddRequest);
+
+		Task<bool> UpdateOrder(OrderUpdateRequest? orderUpdateRequest);
+
+		Task<bool> DeleteOrder(Guid? orderId);
+	}
+}
